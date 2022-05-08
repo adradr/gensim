d51e98db8e5ce0cb1c1d514c75a3fa15f0c7d5bf
adr <adrian.lenard@me.com>
2022-05-09 00:55:31 +0200

    Docker compatibility added with env configuration

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# syntax=docker/dockerfile:1
+
+FROM python:3.9.12-buster
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY gensim/ ./gensim
+copy main.py .
+
+CMD ["python3", "main.py"]
+
+
--- main.py
+++ main.py
@@ -1,41 +1,38 @@
-import logging
-import pickle
-from time import time
 from gensim import *
 from gensim.Enviroment import SelectionCriterias
+import pickle
+import os
 
-# -------- SETTINGS --------
-LOGGING_LEVEL = logging.INFO
-MAP_SIZE = 25
-POPULATION_SIZE = 200
-NUM_STEPS = 100
-NUM_GENERATIONS = 20
-GENE_SIZE = 12
-NUM_INTERNAL_NEURON = 3
-MUTATION_RATE = 0.05
-SELECTION_AREA_SIZE = 0.2
-SELECTION_CRITERIA_TYPE = SelectionCriterias.BOTH_SIDE
-MULTITHREADING_NTHREADS = 200
-# --------------------------
+from dotenv import load_dotenv
+try:
+    load_dotenv('.env')
+except:
+    print('No enviroment file found, will default for os.environ')
 
+import logging
 log = logging.getLogger('gensim')
 logging.basicConfig(format="%(asctime)s - %(message)s")
-log.setLevel(LOGGING_LEVEL)
+log.setLevel(eval(os.environ['LOGGING_LEVEL']))
 
 if (__name__ == "__main__"):
     # Create enviroment
     log.info(f"Creating enviroment...")
-    env = Enviroment.SimEnv(size=MAP_SIZE,
-                            population_size=POPULATION_SIZE,
-                            num_steps=NUM_STEPS,
-                            num_rounds=NUM_GENERATIONS,
-                            gene_size=GENE_SIZE,
-                            num_int_neuron=NUM_INTERNAL_NEURON,
-                            mutation_probability=MUTATION_RATE,
-                            selection_area_width_pct=SELECTION_AREA_SIZE,
-                            criteria_type=SELECTION_CRITERIA_TYPE,
-                            multithreading=MULTITHREADING_NTHREADS)
+    env = Enviroment.SimEnv(size=int(os.environ['MAP_SIZE']),
+                            population_size=int(os.environ['POPULATION_SIZE']),
+                            num_steps=int(os.environ['NUM_STEPS']),
+                            num_rounds=int(os.environ['NUM_GENERATIONS']),
+                            gene_size=int(os.environ['GENE_SIZE']),
+                            num_int_neuron=int(
+                                os.environ['NUM_INTERNAL_NEURON']),
+                            mutation_probability=float(
+                                os.environ['MUTATION_RATE']),
+                            selection_area_width_pct=float(
+                                os.environ['SELECTION_AREA_SIZE']),
+                            criteria_type=eval(
+                                os.environ['SELECTION_CRITERIA_TYPE']),
+                            multithreading=int(os.environ['MULTITHREADING_NTHREADS']))
     log.info(f"Enviroment created: {env.id}")
+    log.info(f"Disclaimer: Timing is only estimate, rounded and not accurate.")
     # Calculate simulation
     env.eval_env()
     log.info(f"Iterating enviroment finished: {env.id}")
